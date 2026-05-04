236702991e25b22f920f174a7300d03fd345cf72
Lilly Schramm <privat@eps-dev.de>
2026-05-04 21:41:39 +0200

    feat: Update Patches

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Locale
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -14,3 +16,38 @@ rootProject.name = "kitty"
 
 include("kitty-api")
 include("kitty-server")
+
+rootProject.name = "kitty"
+for (name in listOf("kitty-api", "kitty-server")) {
+    val projName = name.lowercase(Locale.ENGLISH)
+    include(projName)
+    findProject(":$projName")!!.projectDir = file(name)
+}
+
+fun optionalInclude(name: String, op: (ProjectDescriptor.() -> Unit)? = null) {
+    val settingsFile = file("$name.settings.gradle.kts")
+    if (settingsFile.exists()) {
+        apply(from = settingsFile)
+        findProject(":$name")?.let { op?.invoke(it) }
+    } else {
+        settingsFile.writeText(
+            """
+            // Uncomment to enable the '$name' project
+            // include(":$name")
+
+            """.trimIndent()
+        )
+    }
+}
+
+gradle.lifecycle.beforeProject {
+    val mcVersion = providers.gradleProperty("mcVersion").get().trim()
+    val kittyChannel = providers.gradleProperty("channel").get().trim()
+    val kittyBuildNumber = providers.environmentVariable("BUILD_NUMBER").orNull?.trim()?.toInt()
+    val versionString = if (kittyBuildNumber == null) {
+        "$mcVersion.local-SNAPSHOT"
+    } else {
+        "$mcVersion.build.$kittyBuildNumber-${kittyChannel.lowercase()}"
+    }
+    version = versionString
+}
